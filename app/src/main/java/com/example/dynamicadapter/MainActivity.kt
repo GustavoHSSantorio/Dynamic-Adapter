2aff080ff75a2e3a8fe86260d77df93e7c7fd94b
Gustavo Santorio <gustavo.santorio@xpi.com.br>
2021-05-01 11:12:00 -0300

    com view binding

--- app/src/main/java/com/example/dynamicadapter/MainActivity.kt
+++ app/src/main/java/com/example/dynamicadapter/MainActivity.kt
@@ -4,13 +4,18 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.dynamicadapter.dynamic.presentation.adapter.Dynamic
 import com.dynamicadapter.dynamic.presentation.adapter.DynamicAdapter
+import com.example.dynamicadapter.databinding.LayoutBinding
 
 class MainActivity : AppCompatActivity() {
 
     private val dynamicAdapter : Dynamic = DynamicAdapter()
 
+    lateinit var binding : LayoutBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = LayoutBinding.inflate(layoutInflater)
+        binding.recycler.adapter =  dynamicAdapter as DynamicAdapter
     }
 
 }
