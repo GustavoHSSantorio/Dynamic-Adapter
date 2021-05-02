fa30f313d2dcaba7cd6baade601db6abf2431803
Gustavo Santorio <gustavo.santorio@xpi.com.br>
2021-05-02 10:25:53 -0300

    subindo mudança de nome

--- app/src/main/java/com/example/dynamicadapter/MainActivity.kt
+++ app/src/main/java/com/example/dynamicadapter/MainActivity.kt
@@ -4,17 +4,18 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.dynamicadapter.dynamic.presentation.adapter.Dynamic
 import com.dynamicadapter.dynamic.presentation.adapter.DynamicAdapter
-import com.example.dynamicadapter.databinding.LayoutBinding
+import com.example.dynamicadapter.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private val dynamicAdapter : Dynamic = DynamicAdapter()
 
-    lateinit var binding : LayoutBinding
+    lateinit var binding : ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = LayoutBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
         binding.recycler.adapter =  dynamicAdapter as DynamicAdapter
     }
 
